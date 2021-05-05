687286225cac941470884857fecf0a248855bbbf
Smudger1 <mugoftea37@gmail.com>
2021-05-05 22:38:30 +0100

    commit

---
+++ schema.js
@@ -0,0 +1,40 @@
+const {gql} = require('apollo-server');
+
+const typeDefs = gql `
+    scalar Date
+    
+    type Beacon {
+      beaconAddr: ID!
+      venue: Venue!
+    }
+    
+    type Venue {
+      id: ID!
+      venueName: String!
+    }
+    
+    type CheckIn {
+      id: ID!
+      beacon: Beacon!
+      user: String!
+      dateIn: Date!
+      dateOut: Date
+    }
+    
+    type Query {
+      verifyBeacons(id: [ID]!): [Beacon]
+      checkInsByUser(user: String!): [CheckIn]
+    }
+    
+    type Mutation {
+      createNewCheckIn(beacon: ID!, user: String!): CheckInMutationResponse!
+      updateCheckOut(id: ID!): CheckInMutationResponse!
+    }
+    
+    type CheckInMutationResponse {
+      success: Boolean!
+      message: String
+    }
+`;
+
+module.exports = typeDefs;
